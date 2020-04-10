6428f02b0377433ae1240f4c0bb365149eecebdf
Ben Morrall <bemo56@hotmail.com>
2020-04-09 17:46:36 -0700

    Allow has_selector use in request specs

--- lib/generators/rockstart/rspec/templates/support/test_helpers.rb
+++ lib/generators/rockstart/rspec/templates/support/test_helpers.rb
@@ -3,4 +3,7 @@
 RSpec.configure do |config|
   # Allow direct use of t() and l() in specs
   config.include AbstractController::Translation
+
+  # Allow have_selector matcher in request specs
+  config.include Capybara::RSpecMatchers, type: :request
 end
