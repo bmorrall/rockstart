c3b4a772b54eba6b99504cb3f776c2e1dd05e25c
Ben Morrall <bemo56@hotmail.com>
2020-04-23 14:51:23 -0700

    Add deploy-heroku script to rockstart:deployment:heroku

--- lib/generators/rockstart/deployment/heroku/heroku_generator.rb
+++ lib/generators/rockstart/deployment/heroku/heroku_generator.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 require "rockstart/generators/class_option_helpers"
+require "rockstart/generators/template_helpers"
 
 module Rockstart::Deployment
   class HerokuGenerator < Rails::Generators::Base
     include Rails::Generators::AppName
     include Rockstart::Generators::ClassOptionHelpers
+    include Rockstart::Generators::TemplateHelpers
 
     source_root File.expand_path("templates", __dir__)
 
@@ -17,6 +19,10 @@ module Rockstart::Deployment
 
     def create_app_json
       template "app.json"
+    end
+
+    def add_deploy_script
+      script_template "deploy-heroku"
     end
   end
 end
