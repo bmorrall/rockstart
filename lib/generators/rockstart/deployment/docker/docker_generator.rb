1cdce39adb08d341cd03a3b8452f45788eef8e76
Ben Morrall <bemo56@hotmail.com>
2020-04-26 21:51:06 -0700

    Remove sidekiq reference

--- lib/generators/rockstart/deployment/docker/docker_generator.rb
+++ lib/generators/rockstart/deployment/docker/docker_generator.rb
@@ -24,7 +24,6 @@ module Rockstart::Deployment
     memcached_class_option
     postgres_class_option
     rollbar_class_option
-    sidekiq_class_option
 
     def create_dockerignore
       copy_file "dockerignore", ".dockerignore"
